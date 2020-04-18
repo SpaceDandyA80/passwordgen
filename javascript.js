d1c80a7a81a55cb3f733c0a39a31c09ae7bcb2e8
Guillermo Garcia <rubberducky760@gmail.com>
2020-04-18 14:39:46 -0700

    adding information about code

--- javascript.js
+++ javascript.js
@@ -10,15 +10,16 @@ function writePassword() {
 'Numeric':['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
 'Symbols':['!', '@', '#', '$', '%','^', '&', '*', '(', ')', '_', '+', '/', '<', '>', '?', ';', '\'', '\"']
 }
-
+// empty array 
     let potChar = [];
-
+// prompts and confirms that remain the same because of the const
   const  passLength = prompt('Your password must be at least 8 to 128 characters long');    
   const lowerCase = confirm('Would you like lowercase letters to be included? OK for yes, cancel for no');
   const  upperCase = confirm('Would you like uppercase letters to be included? OK for yes, cancel for no');
   const  Numeric = confirm('Would you like numbers to be included? Ok for yes, cancel for no');
   const  Symbols = confirm('Would you like special characters \"!@#$%\" to be included');
 
+// if confirm name or prompt is true it will concat or append the object array to the array
 if (lowerCase === true){
    potChar = potChar.concat(truly.lowerCase)
 }
@@ -31,22 +32,23 @@ if (Numeric === true){
 if (Symbols === true){
   potChar =  potChar.concat(truly.Symbols)
 }
-console.log(potChar);
-
+// function for generating password
     function generatePassword(){
-
+            // new password is an empty string
         let newPassword = "";
         
-
+            // conditions for new password must be a certain length specifically between 8 and 128 no longer and no shorter
         if (passLength < 8 || passLength > 128){
             alert('your password does not meet requirements for a new generated password');
         }
+        // new password would append the empty array with a random letter uppercase or lower case, number or special character until the number entered has been reached
         else {
             for (i = 0; i < passLength; i++) {
                 newPassword += potChar[Math.floor(Math.random() * potChar.length)];
               
             }
         }
+        // returns new password to password stored in the html
         return newPassword;
     }
  
